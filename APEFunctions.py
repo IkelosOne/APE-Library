16b621c4d288eaf5a5b78093eb6d45cf0c5f9bc5
ArunOsborn <55600843+ArunOsborn@users.noreply.github.com>
2020-08-16 22:51:53 +0100

    Removed IsPrimeLightspeed
    Removed the temporary IsPrimeLightspeed function copied from https://www.rookieslab.com/posts/fastest-way-to-check-if-a-number-is-prime-or-not for temporary use.

--- APEFunctions.py
+++ APEFunctions.py
@@ -11,7 +11,7 @@ import APEFunctions
 ###############################################################
 #A good number to test for primes is 9843498239573897
 
-def IsPrime(n): #This has been optimised to now be 98% the speed of IsPrimeLightspeed (a funciton found on the internet)
+def IsPrime(n): #Version 2
     """Input n, returns True if n is prime and False if not."""
     if n > 1:
         x = 2
@@ -49,27 +49,7 @@ def commaNumber(number):
     while result[0]==",":#Gets rid of commas at start
         result=result[1:]
     return result
-###############################################################
-def IsPrimeLightspeed(n): #https://www.rookieslab.com/posts/fastest-way-to-check-if-a-number-is-prime-or-not
-    """
-    Assumes that n is a positive natural number
-    """
-    # We know 1 is not a prime number
-    if n == 1:
-        return False
 
-    i = 2
-    # This will loop from 2 to int(sqrt(x))
-    while i*i <= n:
-        # Check if i divides x without leaving a remainder
-        if n % i == 0:
-            # This means that n has a factor in between 2 and sqrt(n)
-            # So it is not a prime number
-            return False
-        i += 1
-    # If we did not find any factor in the above loop,
-    # then n is a prime number
-    return True
 ###############################################################
 def ReverseIt(inp): #Takes string or int and return reversed version as string
     inp = str(inp)
