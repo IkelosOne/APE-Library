8c581284e5e77282f5266b83cea83bf2ec5f303d
IkelosOne <pablo.trett@gmail.com>
2020-08-17 13:55:53 +0100

    Intermediary changes

--- APEFunctions.py
+++ APEFunctions.py
@@ -4,18 +4,18 @@
 To import:
 
 import sys
-sys.path.append("E\Example\APE-Library")
+sys.path.append("E/Example/APE-Library")
 import APEFunctions
 """
 
 ###############################################################
 #A good number to test for primes is 9843498239573897
 
-def IsPrime(n): #This has been optimised to now be 98% the speed of IsPrimeLightspeed (a funciton found on the internet)
+def IsPrime(n):  # This has been optimised to now be 98% the speed of IsPrimeLightspeed (a funciton found on the internet)
     """Input n, returns True if n is prime and False if not."""
     if n > 1:
         x = 2
-        while x*x <= n: # x*x<n is much more efficient than x<sqrt(n)
+        while x * x <= n:  # x * x < n is much more efficient than x < sqrt(n)
             if (n % x) == 0:
                 return False
             x += 1
@@ -24,33 +24,33 @@ def IsPrime(n): #This has been optimised to now be 98% the speed of IsPrimeLight
         return False
 
 #print(IsPrime(int(input(""))))
-###############################################################
+
 def makeBool(string):
-    if string=="False":
+    if string == "False":
         return False
-    if string=="True":
+    if string == "True":
         return True
     else:
         print("Impossible")
-###############################################################
+
 def commaNumber(number):
     #Takes number and adds commas every 3 digits. Returns string
-    number=str(number)
-    numList=[]
-    seperates=DIV(len(number),3)
-    for x in range(seperates):#Repeats for the amount of commas
-        leftLim=-(seperates+1-x)*3
-        rightLim=-((seperates-x)*3)
+    number = str(number)
+    numList = []
+    seperates = DIV(len(number), 3)
+    for x in range(seperates):  # Repeats for the amount of commas
+        leftLim = - (seperates + 1 - x) * 3
+        rightLim = - ((seperates - x) * 3)
         numList.append(number[leftLim:rightLim])
     numList.append(number[-3:])
-    result=""
-    for n in range(len(numList)):#Turns list into string
-        result=result+","+numList[n]
-    while result[0]==",":#Gets rid of commas at start
-        result=result[1:]
+    result = ""
+    for n in range(len(numList)):  # Turns list into string
+        result = result + "," + numList[n]
+    while result[0] == ",":  # Gets rid of commas at start
+        result = result[1:]
     return result
-###############################################################
-def IsPrimeLightspeed(n): #https://www.rookieslab.com/posts/fastest-way-to-check-if-a-number-is-prime-or-not
+
+def IsPrimeLightspeed(n):  # https://www.rookieslab.com/posts/fastest-way-to-check-if-a-number-is-prime-or-not
     """
     Assumes that n is a positive natural number
     """
@@ -60,7 +60,7 @@ def IsPrimeLightspeed(n): #https://www.rookieslab.com/posts/fastest-way-to-check
 
     i = 2
     # This will loop from 2 to int(sqrt(x))
-    while i*i <= n:
+    while i * i <= n:
         # Check if i divides x without leaving a remainder
         if n % i == 0:
             # This means that n has a factor in between 2 and sqrt(n)
@@ -70,29 +70,25 @@ def IsPrimeLightspeed(n): #https://www.rookieslab.com/posts/fastest-way-to-check
     # If we did not find any factor in the above loop,
     # then n is a prime number
     return True
-###############################################################
-def ReverseIt(inp): #Takes string or int and return reversed version as string
+
+def ReverseIt(inp):  # Takes string or int and return reversed version as string
     inp = str(inp)
     revNum = ""
     for x in range(len(inp)):
-        digit = inp[len(inp)-x-1]
+        digit = inp[len(inp) - x - 1]
         revNum += digit
     return revNum
-###############################################################
+
 def IsPalendromic(number):
     number = str(number)
-    for x in range (len(number)):
-        if number[x] != number[len(number)-x-1]:
+    for x in range(len(number)):
+        if number[x] != number[len(number) - x - 1]:
             return False
     return True
-###############################################################
-def OccurencesInList(item,list1): #Returns the amount of occurences of an item in a list
+
+def OccurencesInList(item, list1):  # Returns the amount of occurences of an item in a list
     count = 0
     for x in range(len(list1)):
         if list1[x] == item:
             count += 1
     return count
-
-
-
-
