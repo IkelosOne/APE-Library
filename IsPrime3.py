60b16b312b362040fa56064b6d4a1943bb469ee9
ArunOsborn <55600843+ArunOsborn@users.noreply.github.com>
2020-08-17 14:36:06 +0100

    Update IsPrime3.py

--- IsPrime3.py
+++ IsPrime3.py
@@ -5,15 +5,14 @@
 
 primeList = []
 
-def IsPrime(n): #Version 3
+def IsPrime(n): #Version 3 - Time 16.04
     """Input n, returns True if n is prime and False if not."""
     if n > 1:
         x = 2
         while x*x <= n: # x*x<n is much more efficient than x<sqrt(n)
-            if IsPrime(x):
-                if (n % x) == 0:
-                    return False
-            x += 1
+            if (n % x) == 0:
+                return False
+            x += 2
         return True
     else:
         return False
