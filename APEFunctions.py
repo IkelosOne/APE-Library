89e29131411f3e9532eead94e33c708218447ff7
IkelosOne <pablo.trett@gmail.com>
2020-08-17 17:12:01 +0100

    Cleanup
    Moved IsPrime3.py into APEFunctions.py and changed IsPrimeLightSpeed to IsPrime2.

--- APEFunctions.py
+++ APEFunctions.py
@@ -10,26 +10,7 @@ def IsPrime(n):
 	else:
 		return False
 
-def MakeBool(string):
-	if string == "False":
-		return False
-	if string == "True":
-		return True
-	else:
-		print("Impossible")
-
-def InsertCommas(number):
-	"""Inserts commas into the corresponding places in a number."""
-
-	string = str(number)
-	newString = string
-	commasNeeded = len(str(number)[:-1]) // 3
-	while commasNeeded > 0:
-		newString = newString[:-(3 * commasNeeded)] + "," + newString[-(3 * commasNeeded):]
-		commasNeeded -= 1
-	return newString
-
-def IsPrimeLightspeed(n):
+def IsPrime2(n):  # Formerly IsPrimeLightSpeed
 	"""Assumes that n is a positive natural number.
 	Source: https://www.rookieslab.com/posts/fastest-way-to-check-if-a-number-is-prime-or-not"""
 
@@ -50,7 +31,39 @@ def IsPrimeLightspeed(n):
 	# then n is a prime number
 	return True
 
-def ReverseIt(inp):
+def IsPrime3(n):  # Version 3 - Time 16.04
+	"""Input n, returns True if n is prime and False if not."""
+
+	if n > 1:
+		x = 2
+		while x * x <= n:  # x*x<n is much more efficient than x<sqrt(n)
+			if (n % x) == 0:
+				return False
+			x += 2
+		return True
+	else:
+		return False
+
+def MakeBool(string):  # !!!I THINK THIS ALREADY EXISTS IN-BUILT - Pablo
+	if string == "False":
+		return False
+	if string == "True":
+		return True
+	else:
+		print("Impossible")
+
+def InsertCommas(number):
+	"""Inserts commas into the corresponding places in a number."""
+
+	string = str(number)
+	newString = string
+	commasNeeded = len(str(number)[:-1]) // 3
+	while commasNeeded > 0:
+		newString = newString[:-(3 * commasNeeded)] + "," + newString[-(3 * commasNeeded):]
+		commasNeeded -= 1
+	return newString
+
+def ReverseIt(inp):  # !!!I THINK THIS ALREADY EXISTS IN-BUILT - Pablo
 	"""Takes string or int and return reversed version as string."""
 
 	inp = str(inp)
@@ -69,7 +82,7 @@ def IsPalendromic(n):
 			return False
 	return True
 
-def OccurencesInList(item, list1):
+def OccurencesInList(item, list1):  # !!!I THINK THIS ALREADY EXISTS IN-BUILT - Pablo
 	"""Returns the amount of occurences of an item in a list."""
 
 	count = 0
@@ -80,6 +93,7 @@ def OccurencesInList(item, list1):
 
 
 if __name__ == "__main__":
-	"""Use the space below for testing."""
+	"""Use the space below for testing. Any code here will not run when the file is imported as a
+	module. Delete it once you're finished testing."""
 
 	None
