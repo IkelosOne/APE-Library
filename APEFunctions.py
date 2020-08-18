d6b6efa04daa0419199df8aa2b41e1c4a01dc677
ArunOsborn <55600843+ArunOsborn@users.noreply.github.com>
2020-08-18 12:21:56 +0100

    Removed "IsPrime2"
    Formerly IsPrimeLightspeed was only a temporary function that is no longer needed as IsPrime (V2) runs at a similar speed with equal functionality. IsPrime (V2) will be removed in a coming update as Version 3 is almost complete and is >2X as fast!

--- APEFunctions.py
+++ APEFunctions.py
@@ -1,4 +1,4 @@
-def IsPrime(n):
+def IsPrime(n): #Version 2 (will be replaced with version 3 once developent on it ends as bugs may occur)
 	"""Input n, returns True if n is prime and False if not."""
 	if n > 1:
 		x = 2
@@ -10,26 +10,6 @@ def IsPrime(n):
 	else:
 		return False
 
-def IsPrime2(n):  # Formerly IsPrimeLightSpeed
-	"""Assumes that n is a positive natural number.
-	Source: https://www.rookieslab.com/posts/fastest-way-to-check-if-a-number-is-prime-or-not"""
-
-	# We know 1 is not a prime number
-	if n == 1:
-		return False
-
-	i = 2
-	# This will loop from 2 to int(sqrt(x))
-	while i * i <= n:
-		# Check if i divides x without leaving a remainder
-		if n % i == 0:
-			# This means that n has a factor in between 2 and sqrt(n)
-			# So it is not a prime number
-			return False
-		i += 1
-	# If we did not find any factor in the above loop,
-	# then n is a prime number
-	return True
 
 def IsPrime3(n):  # Version 3 - Time 16.04
 	"""Input n, returns True if n is prime and False if not."""
