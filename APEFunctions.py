bb70c02ac3c63c10dc8eff342e333245742c42be
ArunOsborn <55600843+ArunOsborn@users.noreply.github.com>
2020-08-21 22:11:06 +0100

    Added LCM and Dependencies
    A LCM function has been added using FindFactors which has also been added which is a tweaked version of FindNoOfFactors which is added as well. LowestCommonMultipleWIP had some ... bugs. This should be faster for large inputs as it will use the prime factors of the inputs but this is not finished yet so shouldn't be used unless it is to your program.

--- APEFunctions.py
+++ APEFunctions.py
@@ -130,9 +130,81 @@ def Factorial(n):
 		product *= x
 	return product
 ##--------------------------------------------------------------------------------------
+def FindNoOfFactors(number):
+    total = 1
+    for x in range(1,number//2 + 1):
+        if number % x == 0:
+            total += 1
+    return total
+
+def FindFactors(n,prime = False):
+	"""Will return an array of the prime factors in n. If prime is set to true, it will return the prime factor the amount of times it goes into n."""
+
+	n = int(n)
+	factors = []
+	for x in range(1, n//2 + 1):
+		if n % x == 0:
+			if prime:
+				if IsPrime3(x):
+					for i in range(n//x):
+						factors.append(x)
+			else:
+				factors.append(x)
+	return factors
+
+def LowestCommonMultiple(n, m):
+	"""Will return the lowest common multiple of the two inputs"""
+
+	n = int(n)
+	m = int(m)
+	nMultiples = []
+	mMultiples = []
+	count = -1
+	while 0 == 0:
+		count += 1
+		nMultiples.append(n*(count+1))
+		mMultiples.append(m * (count+1))
+		if nMultiples[count] > mMultiples[count]: #  Allows for catchup (stop checking when multiples are far apart maybe idk what I'm doing tbh but it works so it's all good right? I'm gonna listen to some Muse now)
+			for x in range(len(nMultiples)):
+				if mMultiples[count] == nMultiples[x]:
+					return nMultiples[x]
+		else:
+			for y in range(len(mMultiples)):
+				if nMultiples[count] == mMultiples[y]:
+					return mMultiples[y]
+
+def LowestCommonMultipleWIP(n,m):
+	"""Will use prime factors to calculate lcm more efficiently for large inputs"""
+
+	n = int(n)
+	m = int(m)
+	if IsPrime3(n) or IsPrime3(m):
+		return n*m
+	else:
+		pass #  Use an upcoming function to get the factors of each of the numbers
+		nFacList = FindFactors(n,True)
+		mFacList = FindFactors(m,True)
+		combinedFacList = []
+		for x in range(len(nFacList)): #  Attempts to create list of common prime factors, the most amount of times they occur
+			for y in range(len(mFacList)):
+				try:
+					if mFacList[x] == nFacList[y]:
+						commonFactor = mFacList[x]
+						if OccurencesInList(commonFactor,mFacList) > OccurencesInList(commonFactor,nFacList):
+							for i in range(OccurencesInList(commonFactor,mFacList)):
+								combinedFacList.append(commonFactor)
+						else:
+							for i in range(OccurencesInList(commonFactor,nFacList)):
+								combinedFacList.append(commonFactor)
+				except IndexError:
+					pass
+		lcm = 1
+		for t in range(len(combinedFacList)):
+			lcm *= combinedFacList[t]
+		return lcm
 
 if __name__ == "__main__":
 	"""Use the space below for testing. Any code here will not run when the file is imported as a
 	module. Delete it once you're finished testing."""
-	print(Factorial(input("")))
+	print(LowestCommonMultiple(input(""),input("")))
 	None
