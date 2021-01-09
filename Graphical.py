f06f63a7a492aeba12c140d5c70d547554bd6bea
ArunOsborn <55600843+ArunOsborn@users.noreply.github.com>
2021-01-09 17:25:01 +0000

    Fill The Space
    Graph now perfectly fills the space.

--- Graphical.py
+++ Graphical.py
@@ -2,26 +2,28 @@ import turtle
 
 screenWidth =  1600
 screenHeight = 900
+
 marginWidth = 20
 marginHeight = 20
 
 def plotGraph(array): # Imported from terrain gen
-	try:
-		screen = turtle.Screen()
+	screen = turtle.Screen()
 
+	dataRange = getMaxValue(array)-getMinValue(array)
 
-		screen.setup(screenWidth,screenHeight)
-		pen = turtle.Turtle()
-		pen.up()
-		pen.goto((-screenWidth/2) + marginWidth,(-screenHeight/2) + marginHeight)
-		pen.down()
-		for x in range(len(array)):
-			coords = convertCoordsToStandard(x*10,int(array[x])*10)
-			pen.goto(coords[0],coords[1])
-		pen.up()
-		screen.mainloop()
-	except TypeError:
-		pass
+	gapWidth = (screenWidth-marginWidth)/len(array)
+	unitHeight = (screenHeight-marginHeight)/dataRange
+
+	screen.setup(screenWidth,screenHeight)
+	pen = turtle.Turtle()
+	pen.up()
+	pen.goto((-screenWidth/2) + marginWidth,(-screenHeight/2) + marginHeight)
+	pen.down()
+	for x in range(len(array)):
+		coords = convertCoords(x*gapWidth,int(array[x])*unitHeight)
+		pen.goto(coords[0],coords[1])
+	pen.up()
+	screen.mainloop()
 
 def convertCoords(xCoord,yCoord):
 	global screenWidth
@@ -31,5 +33,19 @@ def convertCoords(xCoord,yCoord):
 
 	coords = [(-screenWidth/2) + marginWidth + xCoord,(-screenHeight/2) + marginHeight + yCoord]
 	return coords
+
+def getMaxValue(array):
+	max = array[0]
+	for i in range(len(array)):
+		if array[i]>max:
+			max = array[i]
+	return max
+
+def getMinValue(array):
+	min = array[0]
+	for i in range(len(array)):
+		if array[i]<min:
+			min = array[i]
+	return min
 
 plotGraph([0,1,2,1,3])
