23cac8d7994184da6fdc379e7ad783988d30f23d
ArunOsborn <55600843+ArunOsborn@users.noreply.github.com>
2021-01-09 17:15:03 +0000

    Graph starts from bottom left
    Screen is set to a certain size and the new function "convertCoordsToStandard" returns a list of the co-ordinates to use to plot the graph, taking into account the screen size and margin size.

--- Graphical.py
+++ Graphical.py
@@ -1,16 +1,35 @@
 import turtle
 
+screenWidth =  1600
+screenHeight = 900
+marginWidth = 20
+marginHeight = 20
 
-def GraphicalHeightMap(array): # Imported from terrain gen
-	screen = turtle.Screen()
+def plotGraph(array): # Imported from terrain gen
+	try:
+		screen = turtle.Screen()
 
-	screen.screensize()
-	pen = turtle.Turtle()
-	pen.down()
-	for x in range(len(array)):
-		pen.goto((x*10)-10,int(array[x])*10)
-	pen.up()
-	screen.mainloop()
 
-GraphicalHeightMap([0,1,2,1,3])
-#plotGraph([0,1,2,1,3])
+		screen.setup(screenWidth,screenHeight)
+		pen = turtle.Turtle()
+		pen.up()
+		pen.goto((-screenWidth/2) + marginWidth,(-screenHeight/2) + marginHeight)
+		pen.down()
+		for x in range(len(array)):
+			coords = convertCoordsToStandard(x*10,int(array[x])*10)
+			pen.goto(coords[0],coords[1])
+		pen.up()
+		screen.mainloop()
+	except TypeError:
+		pass
+
+def convertCoordsToStandard(xCoord,yCoord):
+	global screenWidth
+	global screenHeight
+	global marginWidth
+	global marginHeight
+
+	coords = [(-screenWidth/2) + marginWidth + xCoord,(-screenHeight/2) + marginHeight + yCoord]
+	return coords
+
+plotGraph([0,1,2,1,3])
