700d88539e2ef5349a76d1023c63b3da6cf2f759
ArunOsborn <55600843+ArunOsborn@users.noreply.github.com>
2021-01-10 00:03:20 +0000

    Started Implementing Equation Input
    The "equationToArray" function will eventually take a string containing an equation then output an array with the values at the given resolution.

--- Graphical.py
+++ Graphical.py
@@ -1,4 +1,5 @@
 import turtle
+import time
 
 screenWidth =  1600
 screenHeight = 900
@@ -16,6 +17,7 @@ def plotGraph(array): # Imported from terrain gen
 
 	screen.setup(screenWidth,screenHeight)
 	pen = turtle.Turtle()
+	pen.speed = 0
 	pen.up()
 	pen.goto((-screenWidth/2) + marginWidth,(-screenHeight/2) + marginHeight)
 	pen.down()
@@ -48,4 +50,28 @@ def getMinValue(array):
 			min = array[i]
 	return min
 
-plotGraph([0,1,2,1,3])
+def equationToArray(equationString,resolution):
+	# Eventually you will be able to enter an equation in the form y=x...
+	# The resolution is kinda missleading. the line tradjectory is changed every resolution units so large number means fewer updates so longer, straighter lines
+	print("Ahhhhherrrr I'll do this tomorrow")
+	array = [resolution**2+resolution*3+2]
+	gapWidth = (screenWidth-marginWidth)/(screenWidth/resolution)
+	for i in range(round(resolution)):
+		#array.append((resolution*i*screenWidth)**3+2*(resolution*i*screenWidth)**2+1/2*(resolution*i*screenWidth)+2)
+		#array.append(1000/(resolution*i*screenWidth+2))
+		x = gapWidth*i
+		array.append(x**2 + 3*x + 4)
+	return array
+
+if __name__ == "__main__":
+	"""Use the space below for testing. Any code here will not run when the file is imported as a
+	module. Delete it once you're finished testing."""
+
+
+	t0 = time.time()
+	#plotGraph([0,1,2,1,3,4,5,3,2])
+	plotGraph(equationToArray("x",100))
+	t1 = time.time()
+	print("Time required:", t1 - t0)
+
+	None
